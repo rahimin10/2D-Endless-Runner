3709ceb53e2fe27d9ad4accafc936ab40c92e4f4
rahimin10 <rahimin710@gmail.com>
2021-10-27 15:48:37 +0700

    Revisi

---
+++ Assets/Script/ScoreController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreController : MonoBehaviour
+{
+	[Header("Scoring")]
+	public ScoreController score;
+	public float scoringRatio;
+	private float lastPositionX;
+
+    private int currentScore = 0;
+	
+	private void Start()
+	{
+		// reset
+		currentScore = 0;
+	}
+	
+	
+
+
+	public float GetCurrentScore()
+	{
+		return currentScore;
+	}
+	
+	public void IncreaseCurrentScore(int increment)
+	{
+		currentScore += increment;
+	}
+	
+	public void FinishScoring()
+	{
+		// set high score
+		if (currentScore > ScoreData.highScore)
+		{
+			ScoreData.highScore = currentScore;
+		}
+	}
+}
